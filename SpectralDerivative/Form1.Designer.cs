44a9f97c7eab3c0f2bc7dd7c73964807850a1f48
Gabriel Tan <tangabrielkl@gmail.com>
2020-11-28 19:28:57 -0500

    Add project files.

---
+++ SpectralDerivative/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace SpectralDerivative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.SuspendLayout();
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(13, 13);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest.TabIndex = 0;
+            this.buttonTest.Text = "Test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // plotView1
+            // 
+            this.plotView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plotView1.Location = new System.Drawing.Point(13, 43);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(775, 395);
+            this.plotView1.TabIndex = 1;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.plotView1);
+            this.Controls.Add(this.buttonTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTest;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+    }
+}
+
---
+++ SpectralDerivative/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using System.Numerics;
+using DFT2;
+
+namespace SpectralDerivative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            double x_lb = 0;
+            double x_ub = 6;
+
+            double L = x_ub - x_lb;
+
+            int N = 1024;
+
+            Complex[] fvalues = new Complex[N];
+
+            double stepSize = L / N;
+            double x = 0;
+            int i;
+            
+            for(i = 0; x <= 1; i++)
+            {
+                fvalues[i] = new Complex(0,0);
+                x += stepSize;
+            }
+            for(; x <= 3; i++)
+            {
+                fvalues[i] = new Complex(x - 1, 0);
+                x += stepSize;
+            }
+            for (; x <= 5; i++)
+            {
+                fvalues[i] = new Complex(-x + 5, 0);
+                x += stepSize;
+            }
+            for (; x < 6; i++)
+            {
+                fvalues[i] = new Complex(0, 0);
+                x += stepSize;
+            }
+
+            var myModel = new PlotModel { Title = "Example 1" };
+            LineSeries series = new LineSeries();
+            for(i = 0; i < fvalues.Length; i++)
+            {
+                series.Points.Add(new DataPoint(i*stepSize, fvalues[i].Real));
+            }
+            
+            myModel.Series.Add(series);
+            this.plotView1.Model = myModel;
+
+            //derivative calculation
+            var fhat = FFT.Compute(fvalues);
+            Complex[] dfhat = new Complex[fhat.Length];
+            double[] kappa = new double[fhat.Length];
+            Complex imaj = new Complex(0, 1);
+            for(i = 0; i < kappa.Length; i++)
+            {
+                kappa[i] = (2 * Math.PI / L) * ((-N / 2.0) + i);
+            }
+
+            kappa = FFT_Utility<double>.Shift(kappa, (kappa.Length / 2) + 1);
+            for(i = 0; i < dfhat.Length; i++)
+            {
+                dfhat[i] = imaj * kappa[i] * fhat[i];
+            }
+
+            var dfFFT = IFFT.Compute(dfhat);
+
+            LineSeries dfseries = new LineSeries();
+            for (i = 0; i < fvalues.Length; i++)
+            {
+                dfseries.Points.Add(new DataPoint(i * stepSize, dfFFT[i].Real));
+            }
+            myModel.Series.Add(dfseries);
+        }
+    }
+}
